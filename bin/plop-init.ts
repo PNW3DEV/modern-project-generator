f4a701156987037350ae64232e838314bc857baf
desmondpnw3dev <pnw3dev@gmail.com>
2020-09-11 10:49:10 -0700

    add version to init message

--- bin/plop-init.ts
+++ bin/plop-init.ts
@@ -1,7 +1,12 @@
-import chalk from "chalk"
+import chalk from 'chalk'
+
+import { version } from '../package.json'
 
 export const initMessage = () => {
   console.info(
-    `${chalk.green('Welcome to the Modern Project Generator! Please start by selecting a project generator below:')}`
+    `${chalk.green(
+      'Welcome to the Modern Project Generator version',
+      version + '!',
+    )} Please start by selecting a project generator below:`,
   )
 }
--- dist/bin/plop-init.js
+++ dist/bin/plop-init.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initMessage = void 0;
 const chalk_1 = __importDefault(require("chalk"));
+const package_json_1 = require("../package.json");
 exports.initMessage = () => {
-    console.info(`${chalk_1.default.green('Welcome to the Modern Project Generator! Please start by selecting a project generator below:')}`);
+    console.info(`${chalk_1.default.green('Welcome to the Modern Project Generator version', package_json_1.version + '!')} Please start by selecting a project generator below:`);
 };
 //# sourceMappingURL=plop-init.js.map
