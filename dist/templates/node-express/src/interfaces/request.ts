61dc545bfdfb2b3d3a6f9bdae2b6ba26dbe272dd
desmondpnw3dev <pnw3dev@gmail.com>
2020-11-12 15:04:15 -0800

    remove magic stitching of backend services for types

--- dist/templates/node-express/src/interfaces/request.ts
+++ dist/templates/node-express/src/interfaces/request.ts
@@ -3,11 +3,16 @@
 import { Request } from "express"
 import admin from "firebase-admin"
 
+import { DAO } from "../middleware/dao.middleware"
+
 export interface ExtendedRequest extends Request {
-  dao: { [k: string]: { [k: string]: any } }
+  body: Request.Body
+  dao: DAO | { [k: string]: { [k: string]: any } }
   user: admin.auth.DecodedIdToken
   log: any
   token?: string
+  headers: Request.Headers
+  ip: Request.Ip
 }
 
 export default interface RequestSchema extends ExtendedRequest {}
