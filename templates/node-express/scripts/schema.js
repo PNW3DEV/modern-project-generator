3949a0428cda96ef51b5eb034ebbf280cc30c1a2
Desmond G. Jones <desmond@pnw3dev.com>
2020-12-22 15:26:16 -0800

    bugfix/node-express-interfaces (#4)

--- templates/node-express/scripts/schema.js
+++ templates/node-express/scripts/schema.js
@@ -1,31 +1,34 @@
 const tsj = require("ts-to-json")
-const fs = require("fs")
+const { promises: fs } = require("fs")
 const pascalCase = require('pascalcase')
 
 module.exports = (async () => {
   try {
-    fileNames = fs.readdirSync(__dirname + '/../src/interfaces')
+    fileNames = await fs.readdir(__dirname + '/../src/interfaces')
     fileNames.forEach(fileName => {
       if (fileName.includes('.interface.ts')) {
         const name = pascalCase(fileName.split('.')[0])
         const config = {
-            path: `../src/interfaces/${fileName}`,
-            tsconfig: __dirname + "/tsconfig.json",
-            type: `${name}Schema`,
-            expose: "export",
-            jsDoc: "extended",
-            topRef: true
+          path: `${__dirname}/../src/interfaces/${fileName}`,
+          tsconfig: __dirname + "/tsconfig.json",
+          type: `${name}Schema`,
+          expose: "export",
+          jsDoc: "extended",
+          topRef: true,
+          ignoreMultipleDefinitions: true,
         }
+
         const output = __dirname + `/../src/schema/${name}.schema.json`.toLowerCase()
         const schema = tsj.createGenerator(config).createSchema(config.type)
         const schemaString = JSON.stringify(schema, null, 2)
         fs.writeFile(output, schemaString, (err) => {
-            if (err) throw err
-            console.info(`Generated JSON Schema ${output}`)
+          if (err) throw err
+          console.info(`Generated JSON Schema ${output}`)
         })
       }
     })
     console.info('Successfully Generated JSON Schema')
+
   } catch (e) {
     console.error("error generating schema", e)
   }
--- templates/node-express/src/interfaces/request.ts
+++ templates/node-express/src/interfaces/request.ts
@@ -1,18 +1,14 @@
 /* This file does not include .interface to bypass JSON Schema Generation */
-
 import { Request } from "express"
 import admin from "firebase-admin"
 
 import { DAO } from "../middleware/dao.middleware"
 
 export interface ExtendedRequest extends Request {
-  body: Request.Body
-  dao: DAO | { [k: string]: { [k: string]: any } }
+  dao: DAO
   user: admin.auth.DecodedIdToken
   log: any
   token?: string
-  headers: Request.Headers
-  ip: Request.Ip
 }
 
 export default interface RequestSchema extends ExtendedRequest {}
